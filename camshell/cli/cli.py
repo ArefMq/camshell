a8cede44d8d5af32434f6ee2875bfbfc3c7bc0c6
Aref Mehr <aref.mehr@boostinc.com>
2025-02-04 12:21:03 +0000

    remove dead code from cli

--- camshell/cli/cli.py
+++ camshell/cli/cli.py
@@ -3,9 +3,7 @@ import platform
 import click
 
 from camshell.camshell_core import CamShell
-from camshell.display.curses_screen import CursesScreen, CursesScreenImproved
-from camshell.display.ui_screen import UIScreen
-from camshell.vision import FakeCamera
+from camshell.display.curses_screen import CursesScreenImproved
 from camshell.vision.camera import MacosCamera
 
 
@@ -21,12 +19,9 @@ def cli(cap_id: str | None):
     if cap_id is None:
         cap_id = 0 if platform.system() == "Darwin" else "/dev/video0"
     try:
-        # camera = FakeCamera(cap_id)
         camera = MacosCamera(device_index=0, max_rate=5)
-        
-        # display = UIScreen()
         display = CursesScreenImproved()
-        
+
         cs = CamShell(camera, display)
         cs.initialize()
         cs.run()
